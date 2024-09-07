83d90136503648b47ec19cd7c3b53aeb81358827
Sha Kong-Brooks <sha.kongbrooks@gmail.com>
2024-09-08 08:36:00 +1000

    Updated priceRegexes list

--- currency.js
+++ currency.js
@@ -1,38 +1,38 @@
 function extractPrice(text) {
   const priceRegexes = [
-    { regex: /د\.إ\d+(\.\d{2})?/, currency: "AED" }, // د.إ - UAE Dirham
-    { regex: /ARS\s?\d+(\.\d{2})?/, currency: "ARS" }, // ARS - Argentine Peso
-    { regex: /\d{1,3}(,\d{3})*(\.\d{2})?\s?AUD/, currency: "AUD" },
-    { regex: /R\$\d+(\.\d{2})?/, currency: "BRL" }, // R$ - Brazilian Real
-    { regex: /\$\d+(\.\d{2})?/, currency: "CAD" }, // CAD - Canadian Dollar
-    { regex: /CHF\s?\d+(\.\d{2})?/, currency: "CHF" }, // CHF - Swiss Franc
-    { regex: /CZK\s?\d+(\.\d{2})?/, currency: "CZK" }, // CZK - Czech Koruna
-    { regex: /DKK\s?\d+(\.\d{2})?/, currency: "DKK" }, // DKK - Danish Krone
-    { regex: /€\d+(\.\d{2})?/, currency: "EUR" }, // € - Euro
-    { regex: /£\d+(\.\d{2})?/, currency: "GBP" }, // £ - British Pound
-    { regex: /\₱\d+(\.\d{2})?/, currency: "PHP" }, // ₱ - Philippine Peso
-    { regex: /HK\$\d+(\.\d{2})?/, currency: "HKD" }, // HK$ - Hong Kong Dollar
-    { regex: /HUF\s?\d+(\.\d{2})?/, currency: "HUF" }, // HUF - Hungarian Forint
-    { regex: /₹\d+(\.\d{2})?/, currency: "INR" }, // ₹ - Indian Rupee
-    { regex: /ILS\s?\d+(\.\d{2})?/, currency: "ILS" }, // ₪ - Israeli Shekel
-    { regex: /ISK\s?\d+(\.\d{2})?/, currency: "ISK" }, // ISK - Icelandic Króna
-    { regex: /\d{1,3}(,\d{3})*(\.\d{2})?\s?JPY/, currency: "JPY" },
-    { regex: /₩\d+(\.\d{2})?/, currency: "KRW" }, // ₩ - South Korean Won
-    { regex: /\d+(\.\d{2})?\s?MXN/, currency: "MXN" }, // MXN - Mexican Peso
-    { regex: /NOK\s?\d+(\.\d{2})?/, currency: "NOK" }, // NOK - Norwegian Krone
-    { regex: /₦\d+(\.\d{2})?/, currency: "NGN" }, // ₦ - Nigerian Naira
-    { regex: /NZ\$\d+(\.\d{2})?/, currency: "NZD" }, // NZ$ - New Zealand Dollar
-    { regex: /PLN\s?\d+(\.\d{2})?/, currency: "PLN" }, // PLN - Polish Zloty
-    { regex: /₽\d+(\.\d{2})?/, currency: "RUB" }, // ₽ - Russian Ruble
-    { regex: /R\d+(\.\d{2})?/, currency: "ZAR" }, // R - South African Rand
-    { regex: /SAR\s?\d+(\.\d{2})?/, currency: "SAR" }, // SAR - Saudi Riyal
-    { regex: /\d+(\.\d{2})?\s?SEK/, currency: "SEK" }, // SEK - Swedish Krona
-    { regex: /S\$\d+(\.\d{2})?/, currency: "SGD" }, // S$ - Singapore Dollar
-    { regex: /\d+(\.\d{2})?\s?THB/, currency: "THB" }, // ฿ - Thai Baht
-    { regex: /TL\s?\d+(\.\d{2})?/, currency: "TRY" }, // TL - Turkish Lira
-    { regex: /\d{1,3}(,\d{3})*(\.\d{2})?\s?USD/, currency: "USD" },
-    { regex: /₫\d+(\.\d{2})?/, currency: "VND" }, // ₫ - Vietnamese Dong
-    { regex: /R\d+(\.\d{2})?/, currency: "ZAR" }, // R - South African Rand
+    { regex: /د\.إ\d+(\.\d{2})?/, currency: "AED" }, // AED (UAE Dirham) - symbol: د.إ
+    { regex: /ARS\s?\d+(\.\d{2})?/, currency: "ARS" }, // ARS (Argentine Peso) - abbreviation: ARS
+    { regex: /\d{1,3}(,\d{3})*(\.\d{2})?\s?AUD/, currency: "AUD" }, // AUD (Australian Dollar) - abbreviation: AUD
+    { regex: /R\$\d+(\.\d{2})?/, currency: "BRL" }, // BRL (Brazilian Real) - symbol: R$
+    { regex: /\$\d+(\.\d{2})?/, currency: "CAD" }, // CAD (Canadian Dollar) - symbol: $
+    { regex: /CHF\s?\d+(\.\d{2})?/, currency: "CHF" }, // CHF (Swiss Franc) - abbreviation: CHF
+    { regex: /CZK\s?\d+(\.\d{2})?/, currency: "CZK" }, // CZK (Czech Koruna) - abbreviation: CZK
+    { regex: /DKK\s?\d+(\.\d{2})?/, currency: "DKK" }, // DKK (Danish Krone) - abbreviation: DKK
+    { regex: /€\d+(\.\d{2})?/, currency: "EUR" }, // EUR (Euro) - symbol: €
+    { regex: /£\d+(\.\d{2})?/, currency: "GBP" }, // GBP (British Pound) - symbol: £
+    { regex: /\₱\d+(\.\d{2})?/, currency: "PHP" }, // PHP (Philippine Peso) - symbol: ₱
+    { regex: /HK\$\d+(\.\d{2})?/, currency: "HKD" }, // HKD (Hong Kong Dollar) - symbol: HK$
+    { regex: /HUF\s?\d+(\.\d{2})?/, currency: "HUF" }, // HUF (Hungarian Forint) - abbreviation: HUF
+    { regex: /₹\d+(\.\d{2})?/, currency: "INR" }, // INR (Indian Rupee) - symbol: ₹
+    { regex: /ILS\s?\d+(\.\d{2})?/, currency: "ILS" }, // ILS (Israeli Shekel) - abbreviation: ILS
+    { regex: /ISK\s?\d+(\.\d{2})?/, currency: "ISK" }, // ISK (Icelandic Króna) - abbreviation: ISK
+    { regex: /\d{1,3}(,\d{3})*(\.\d{2})?\s?JPY/, currency: "JPY" }, // JPY (Japanese Yen) - abbreviation: JPY
+    { regex: /₩\d+(\.\d{2})?/, currency: "KRW" }, // KRW (South Korean Won) - symbol: ₩
+    { regex: /\d+(\.\d{2})?\s?MXN/, currency: "MXN" }, // MXN (Mexican Peso) - abbreviation: MXN
+    { regex: /NOK\s?\d+(\.\d{2})?/, currency: "NOK" }, // NOK (Norwegian Krone) - abbreviation: NOK
+    { regex: /₦\d+(\.\d{2})?/, currency: "NGN" }, // NGN (Nigerian Naira) - symbol: ₦
+    { regex: /NZ\$\d+(\.\d{2})?/, currency: "NZD" }, // NZD (New Zealand Dollar) - symbol: NZ$
+    { regex: /PLN\s?\d+(\.\d{2})?/, currency: "PLN" }, // PLN (Polish Zloty) - abbreviation: PLN
+    { regex: /₽\d+(\.\d{2})?/, currency: "RUB" }, // RUB (Russian Ruble) - symbol: ₽
+    { regex: /R\d+(\.\d{2})?/, currency: "ZAR" }, // ZAR (South African Rand) - symbol: R
+    { regex: /SAR\s?\d+(\.\d{2})?/, currency: "SAR" }, // SAR (Saudi Riyal) - abbreviation: SAR
+    { regex: /\d+(\.\d{2})?\s?SEK/, currency: "SEK" }, // SEK (Swedish Krona) - abbreviation: SEK
+    { regex: /S\$\d+(\.\d{2})?/, currency: "SGD" }, // SGD (Singapore Dollar) - symbol: S$
+    { regex: /\d+(\.\d{2})?\s?THB/, currency: "THB" }, // THB (Thai Baht) - symbol: ฿
+    { regex: /TL\s?\d+(\.\d{2})?/, currency: "TRY" }, // TRY (Turkish Lira) - abbreviation: TL
+    { regex: /\d{1,3}(,\d{3})*(\.\d{2})?\s?USD/, currency: "USD" }, // USD (US Dollar) - abbreviation: USD
+    { regex: /₫\d+(\.\d{2})?/, currency: "VND" }, // VND (Vietnamese Dong) - symbol: ₫
+    { regex: /R\d+(\.\d{2})?/, currency: "ZAR" }, // ZAR (South African Rand) - symbol: R
   ];
 
   for (const { regex, currency } of priceRegexes) {
